4b0e8139cf399ad2ea26e83d446fc31e69a4e5b4
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-28 19:10:34 +0530

    Add initial pipeline setup with index, package, and test files

---
+++ pipeline/index.js
@@ -0,0 +1,2 @@
+console.log("Hello, World! This is the pipeline index file.");
+console.log("I am learning how to set up a CI/CD pipeline for my project. using GitHub Actions");
---
+++ pipeline/test.js
@@ -0,0 +1,5 @@
+console.log("Starting tests ...");
+setTimeout(() => {
+    console.log("Waiting for 3 seconds before running tests...");
+}, 3000);
+console.log("Tests completed successfully!");
